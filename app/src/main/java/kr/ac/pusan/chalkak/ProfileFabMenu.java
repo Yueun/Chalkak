6a9b007500a8624ef4c3f97b39a3fc4d5b95519f
Yueun <yueuns@gmail.com>
2018-09-13 04:34:38 +0900

    Add Cognitive Face

---
+++ app/src/main/java/kr/ac/pusan/chalkak/ProfileFabMenu.java
@@ -0,0 +1,298 @@
+package kr.ac.pusan.chalkak;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Emotion;
+import com.microsoft.projectoxford.face.contract.Face;
+import com.microsoft.projectoxford.face.contract.FaceRectangle;
+import com.microsoft.projectoxford.face.contract.Hair;
+import com.microsoft.projectoxford.face.contract.Makeup;
+
+import kr.ac.pusan.chalkak.utils.Tools;
+
+public class ProfileFabMenu extends AppCompatActivity {
+    private final int PICK_IMAGE = 1;
+    private ProgressDialog detectionProgressDialog;
+
+    private final String apiEndpoint = "https://japaneast.api.cognitive.microsoft.com/face/v1.0";
+    private final String subscriptionKey = "44a1c74666714a0e90bafd186472b487";
+
+    private final FaceServiceClient faceServiceClient =
+            new FaceServiceRestClient(apiEndpoint, subscriptionKey);
+
+    private TextView textAge, textSmile, textGender, textEmotion, textMakeup, textHair, textGlass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_fab_menu);
+
+        // initToolbar();
+
+        detectionProgressDialog = new ProgressDialog(this);
+        detectAndFrame(BitmapFactory.decodeResource(getResources(), R.drawable.image_2));
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle("Profile");
+        Tools.setSystemBarColor(this, R.color.deep_orange_500);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Detect faces by uploading a face image.
+    // Frame faces after detection.
+    private void detectAndFrame(final Bitmap imageBitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream =
+                new ByteArrayInputStream(outputStream.toByteArray());
+
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>() {
+                    String exceptionMessage = "";
+
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("결과 조회 중...");
+                            Face[] result = faceServiceClient.detect(
+                                    params[0],
+                                    true,         // returnFaceId
+                                    false,        // returnFaceLandmarks
+                                    // returnFaceAttributes:
+                                    new FaceServiceClient.FaceAttributeType[] {
+                                            FaceServiceClient.FaceAttributeType.Age,
+                                            FaceServiceClient.FaceAttributeType.Smile,
+                                            FaceServiceClient.FaceAttributeType.Gender,
+                                            FaceServiceClient.FaceAttributeType.Emotion,
+                                            FaceServiceClient.FaceAttributeType.Makeup,
+                                            FaceServiceClient.FaceAttributeType.Hair,
+                                            FaceServiceClient.FaceAttributeType.Glasses
+                                    }
+                            );
+
+                            if (result == null){
+                                publishProgress(
+                                        "Detection Finished. Nothing detected");
+                                return null;
+                            }
+                            publishProgress(String.format(
+                                    "Detection Finished. %d face(s) detected",
+                                    result.length));
+                            return result;
+                        } catch (Exception e) {
+                            exceptionMessage = String.format(
+                                    "Detection failed: %s", e.getMessage());
+                            return null;
+                        }
+                    }
+
+                    @Override
+                    protected void onPreExecute() {
+                        //TODO: show progress dialog
+                        detectionProgressDialog.show();
+                    }
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        //TODO: update progress
+                        detectionProgressDialog.setMessage(progress[0]);
+                    }
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+                        //TODO: update face frames
+                        detectionProgressDialog.dismiss();
+                        if(!exceptionMessage.equals("")){
+                            showError(exceptionMessage);
+                        }
+                        if (result == null) return;
+                        ImageView imageView = findViewById(R.id.imageSame);
+                        imageView.setImageBitmap(
+                                drawFaceRectanglesOnBitmap(imageBitmap, result));
+                        imageBitmap.recycle();
+
+                        List<Face> faces = new ArrayList<>();
+                        faces = Arrays.asList(result);
+
+                        String age = Double.toString(faces.get(0).faceAttributes.age);
+                        String smile = Double.toString(faces.get(0).faceAttributes.smile);
+                        String gender = faces.get(0).faceAttributes.gender;
+                        String emotion = getEmotion(faces.get(0).faceAttributes.emotion);
+                        String makeup = getMakeup(faces.get(0).faceAttributes.makeup);
+                        String hair = getHair(faces.get(0).faceAttributes.hair);
+
+                        textAge = findViewById(R.id.textAge);
+                        textSmile = findViewById(R.id.textSmile);
+                        textGender = findViewById(R.id.textGender);
+                        textEmotion = findViewById(R.id.textEmotion);
+                        textMakeup = findViewById(R.id.textMakeup);
+                        textHair = findViewById(R.id.textHair);
+
+                        textAge.setText(age);
+                        textSmile.setText(smile);
+                        textGender.setText(gender);
+                        textEmotion.setText(emotion);
+                        textMakeup.setText(makeup);
+                        textHair.setText(hair);
+                    }
+
+                    private String getHair(Hair hair) {
+                        if (hair.hairColor.length == 0)
+                        {
+                            if (hair.invisible)
+                                return "Invisible";
+                            else
+                                return "Bald";
+                        }
+                        else
+                        {
+                            int maxConfidenceIndex = 0;
+                            double maxConfidence = 0.0;
+
+                            for (int i = 0; i < hair.hairColor.length; ++i)
+                            {
+                                if (hair.hairColor[i].confidence > maxConfidence)
+                                {
+                                    maxConfidence = hair.hairColor[i].confidence;
+                                    maxConfidenceIndex = i;
+                                }
+                            }
+
+                            return hair.hairColor[maxConfidenceIndex].color.toString();
+                        }
+                    }
+
+                    private String getMakeup(Makeup makeup) {
+                        return  (makeup.eyeMakeup || makeup.lipMakeup) ? "Yes" : "No" ;
+                    }
+
+                    private String getEmotion(Emotion emotion)
+                    {
+                        String emotionType = "";
+                        double emotionValue = 0.0;
+                        if (emotion.anger > emotionValue)
+                        {
+                            emotionValue = emotion.anger;
+                            emotionType = "Anger";
+                        }
+                        if (emotion.contempt > emotionValue)
+                        {
+                            emotionValue = emotion.contempt;
+                            emotionType = "Contempt";
+                        }
+                        if (emotion.disgust > emotionValue)
+                        {
+                            emotionValue = emotion.disgust;
+                            emotionType = "Disgust";
+                        }
+                        if (emotion.fear > emotionValue)
+                        {
+                            emotionValue = emotion.fear;
+                            emotionType = "Fear";
+                        }
+                        if (emotion.happiness > emotionValue)
+                        {
+                            emotionValue = emotion.happiness;
+                            emotionType = "Happiness";
+                        }
+                        if (emotion.neutral > emotionValue)
+                        {
+                            emotionValue = emotion.neutral;
+                            emotionType = "Neutral";
+                        }
+                        if (emotion.sadness > emotionValue)
+                        {
+                            emotionValue = emotion.sadness;
+                            emotionType = "Sadness";
+                        }
+                        if (emotion.surprise > emotionValue)
+                        {
+                            emotionValue = emotion.surprise;
+                            emotionType = "Surprise";
+                        }
+                        return String.format("%s: %f", emotionType, emotionValue);
+                    }
+                };
+
+        detectTask.execute(inputStream);
+    }
+
+    private void showError(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }})
+                .create().show();
+    }
+
+    private static Bitmap drawFaceRectanglesOnBitmap(
+            Bitmap originalBitmap, Face[] faces) {
+        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(10);
+        if (faces != null) {
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+            }
+        }
+        return bitmap;
+    }
+}
---
+++ app/src/main/java/kr/ac/pusan/chalkak/SliderImageCardAuto.java
@@ -0,0 +1,347 @@
+package kr.ac.pusan.chalkak;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.balysv.materialripple.MaterialRippleLayout;
+
+import kr.ac.pusan.chalkak.adapter.AdapterPeople;
+import kr.ac.pusan.chalkak.data.DataGenerator;
+import kr.ac.pusan.chalkak.model.Image;
+import kr.ac.pusan.chalkak.model.People;
+import kr.ac.pusan.chalkak.utils.Tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SliderImageCardAuto extends AppCompatActivity {
+
+    private View parent_view;
+    private ViewPager viewPager;
+    private LinearLayout layout_dots;
+    private AdapterImageSlider adapterImageSlider;
+    private Runnable runnable = null;
+    private Handler handler = new Handler();
+
+    private RecyclerView recyclerView;
+    private AdapterPeople adapter;
+
+    private BottomSheetBehavior mBehavior;
+    private BottomSheetDialog mBottomSheetDialog;
+    private View bottom_sheet;
+
+    private Button buttonVote;
+
+    private static int[] array_image_place = {
+            R.drawable.image_12,
+            R.drawable.image_13,
+            R.drawable.image_14,
+            R.drawable.image_15,
+            R.drawable.image_8,
+    };
+
+    private static String[] array_title_place = {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+    };
+
+    private static String[] array_brief_place = {
+            "설명1",
+            "설명2",
+            "설명3",
+            "설명4",
+            "설명5",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider_image_card_auto);
+
+        // initToolbar();
+        initComponent();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Places");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initComponent() {
+        layout_dots = (LinearLayout) findViewById(R.id.layout_dots);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        adapterImageSlider = new AdapterImageSlider(this, new ArrayList<Image>());
+
+        Intent intent = getIntent();
+
+        final List<Image> items = new ArrayList<>();
+        Image obj = new Image();
+        obj.image = intent.getIntExtra("image", 0);
+        obj.imageDrw = getResources().getDrawable(obj.image);
+        obj.name = "HashTag";
+        obj.brief = "Location";
+        items.add(obj);
+
+        /* for (int i = 0; i < array_image_place.length; i++) {
+            Image obj = new Image();
+            // obj.image = array_image_place[i];
+            obj.image = intent.getIntExtra("image", 0);
+            obj.imageDrw = getResources().getDrawable(obj.image);
+            obj.name = array_title_place[i];
+            obj.brief = array_brief_place[i];
+            items.add(obj);
+        } */
+
+        adapterImageSlider.setItems(items);
+        viewPager.setAdapter(adapterImageSlider);
+
+        // displaying selected image first
+        viewPager.setCurrentItem(0);
+        addBottomDots(layout_dots, adapterImageSlider.getCount(), 0);
+        ((TextView) findViewById(R.id.title)).setText(items.get(0).name);
+        ((TextView) findViewById(R.id.brief)).setText(items.get(0).brief);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int pos, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int pos) {
+                ((TextView) findViewById(R.id.title)).setText(items.get(pos).name);
+                ((TextView) findViewById(R.id.brief)).setText(items.get(pos).brief);
+                addBottomDots(layout_dots, adapterImageSlider.getCount(), pos);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        startAutoSlider(adapterImageSlider.getCount());
+
+        /*
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //set data and list adapter
+        adapter = new AdapterPeople(this, DataGenerator.getPeopleData(this));
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new AdapterPeople.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, People obj, int pos) {
+                showBottomSheetDialog(obj);
+            }
+        });
+        */
+
+        bottom_sheet = findViewById(R.id.bottom_sheet);
+        mBehavior = BottomSheetBehavior.from(bottom_sheet);
+
+        buttonVote = (Button) findViewById(R.id.button_vote);
+        buttonVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
+    }
+
+    private void addBottomDots(LinearLayout layout_dots, int size, int current) {
+        ImageView[] dots = new ImageView[size];
+
+        layout_dots.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new ImageView(this);
+            int width_height = 15;
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(width_height, width_height));
+            params.setMargins(10, 0, 10, 0);
+            dots[i].setLayoutParams(params);
+            dots[i].setImageResource(R.drawable.shape_circle_outline);
+            dots[i].setColorFilter(ContextCompat.getColor(this, R.color.grey_40), PorterDuff.Mode.SRC_ATOP);
+            layout_dots.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[current].setImageResource(R.drawable.shape_circle);
+            dots[current].setColorFilter(ContextCompat.getColor(this, R.color.grey_40), PorterDuff.Mode.SRC_ATOP);
+        }
+    }
+
+    private void startAutoSlider(final int count) {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int pos = viewPager.getCurrentItem();
+                pos = pos + 1;
+                if (pos >= count) pos = 0;
+                viewPager.setCurrentItem(pos);
+                handler.postDelayed(runnable, 3000);
+            }
+        };
+        handler.postDelayed(runnable, 3000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private static class AdapterImageSlider extends PagerAdapter {
+
+        private Activity act;
+        private List<Image> items;
+
+        private AdapterImageSlider.OnItemClickListener onItemClickListener;
+
+        private interface OnItemClickListener {
+            void onItemClick(View view, Image obj);
+        }
+
+        public void setOnItemClickListener(AdapterImageSlider.OnItemClickListener onItemClickListener) {
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        // constructor
+        private AdapterImageSlider(Activity activity, List<Image> items) {
+            this.act = activity;
+            this.items = items;
+        }
+
+        @Override
+        public int getCount() {
+            return this.items.size();
+        }
+
+        public Image getItem(int pos) {
+            return items.get(pos);
+        }
+
+        public void setItems(List<Image> items) {
+            this.items = items;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((RelativeLayout) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            final Image o = items.get(position);
+            LayoutInflater inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = inflater.inflate(R.layout.item_slider_image, container, false);
+
+            ImageView image = (ImageView) v.findViewById(R.id.image);
+            MaterialRippleLayout lyt_parent = (MaterialRippleLayout) v.findViewById(R.id.lyt_parent);
+            Tools.displayImageOriginal(act, image, o.image);
+            lyt_parent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View v) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemClick(v, o);
+                    }
+                }
+            });
+
+            ((ViewPager) container).addView(v);
+
+            return v;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView((RelativeLayout) object);
+
+        }
+
+    }
+
+    private void showBottomSheetDialog(/*final People people*/) {
+        if (mBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+            mBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
+
+        final View view = getLayoutInflater().inflate(R.layout.sheet_basic, null);
+        // ((TextView) view.findViewById(R.id.name)).setText(people.name);
+        ((TextView) view.findViewById(R.id.address)).setText(R.string.middle_lorem_ipsum);
+        (view.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomSheetDialog.dismiss();
+            }
+        });
+
+        (view.findViewById(R.id.bt_details)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Details clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mBottomSheetDialog = new BottomSheetDialog(this);
+        mBottomSheetDialog.setContentView(view);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mBottomSheetDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        mBottomSheetDialog.show();
+        mBottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                mBottomSheetDialog = null;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        if (runnable != null) handler.removeCallbacks(runnable);
+        super.onDestroy();
+    }
+}
